a3020326ae6f70edde71d486ee50a016ba9397f4
Saif <ninja_saif@hotmail.com>
2024-12-06 10:56:40 +0100

    fix issue and add hover styling

--- scripts/all-webinars.js
+++ scripts/all-webinars.js
@@ -1,17 +1,17 @@
 //querry select alle "a" van class ".subject-boxes"
-document.querySelectorAll(".subject-boxes a").forEach((a) => {
+document.querySelectorAll(".subject-boxes button").forEach((button) => {
 
   //voor elke a voeg een click eventlistener functie toe
-  a.addEventListener("click", () => {
+  button.addEventListener("click", () => {
     // categoryName variable = alle a attribute met "data-category"
-    let categoryName = a.getAttribute("data-category");
+    let categoryName = button.getAttribute("data-category");
 
     //toon of verberg webinars
-    let activeWebinars = document.querySelectorAll(".webinar-item");
+    let allWebinars = document.querySelectorAll(".webinar-item");
     let hasActiveWebinars = false;
 
-    //voor elke actieve webinar voer deze functie uit
-    activeWebinars.forEach((webinarItem) => {
+    //voor elke webinar voer deze functie uit
+    allWebinars.forEach((webinarItem) => {
       if (webinarItem.getAttribute("data-category") === categoryName) {
         webinarItem.classList.add("active");
         hasActiveWebinars = true; // Er is een actieve webinar
